c438df9269f9869a43999630f069412d99d8a8a2
Ailea Patrinely <aileapatrinely@gmail.com>
2021-08-05 15:50:45 -0500

    creates forms and error display on index.php

--- index.php
+++ index.php
@@ -10,6 +10,45 @@ session_start();
     <title>The Wall Login/Registration</title>
 </head>
 <body>
-    <--form goes here-->
+    <!-- div for Registration -->
+    <div>
+        <h1>Registration</h1>
+<?php if(isset($_SESSION['reg_errors'])){
+    foreach($_SESSION['reg_errrors'] as $error){?>
+    <p><?=$error?></p>
+<?php
+    }
+}?>
+    <form action="process.php" method="post">
+        <label for="first_name">First Name</label>
+        <input id="first_name" type="text" name="first_name">
+        <label for="last_name">Last Name</label>
+        <input id="last_name" type="text" name="last_name">
+        <label for="email">Email</label>
+        <input id="email" type="text" name="email">
+        <label for="password">Password</label>
+        <input id="password" type="text" name="password">
+        <label for="c_password">Confirm Password</label>
+        <input id="c_password" type="text" name="c_password">
+        <input id="submit" value="register">
+    </form>
+    </div>
+<!-- div for Login -->
+    <div>
+        <h1>Login</h1>
+<?php if(isset($_SESSION['login_errors'])){
+    foreach($_SESSION['login_errors'] as $error){?>
+        <p><?=$error?></p>
+<?php
+    }
+}   ?>
+    <form action="process.php" method="post">
+        <label for="email">Email</label>
+        <input id="email" type="text" name='email'>
+        <label for="password">Password</label>
+        <input id="password" type="text" name="password">
+        <input type="submit" value="Login">
+    </form>
+</div>
 </body>
 </html>
