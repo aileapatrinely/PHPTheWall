4119c0ddbde27706f49a8b3bdf576ea2ca4bea63
Ailea Patrinely <aileapatrinely@gmail.com>
2021-08-05 15:57:28 -0500

    creates registration, login, logout, and no form logic in process.php

--- process.php
+++ process.php
@@ -5,8 +5,156 @@ $_SESSION['reg-errors']=[];
 $_SESSION['login-errors']=[];
 
 //registration form
+// if statement check isset and not empty
+if(isset($_POST['action'])&& $_POST['action']=='register'){
+    if(!empty($_POST['first_name'])){
+        // set errors
+        $_SESSION['reg_errors'][]="First Name is required.";
+    }
+    //else if ctype alpha for first name
+    else if(!ctype_alpha($_POST['first_name'])){
+        // set errors
+        $_SESSION['reg_errors'][]="First Name cannot have any numbers.";
+    }
+    //else if strlen
+    else if(strlen($_POST['first_name'])<2){
+        $_SESSION['reg_errors'][]="First Name has to be longer than 2 characters, if not, go get that legally changed, then try again.";
+    }
+    //if not last name
+    if(!$_POST['last_name']){
+        //set error
+        $_SESSION['reg_errors'][]="Last Name is required.";
+    }
+    //else if ctype alpha last name
+    else if(!ctype_alpha($_POST['last_name'])){
+        //errors
+        $_SESSION['reg_errors'][]="Last Name cannot have any numbers.";
+    }
+    //else if strlen
+    else if(strlen($_POST['last_name'])<2){
+        $_SESSION['reg_errors'][]="Last Name has to be longer than 2 characters, if not, go get that legally changed, then try again.";
+    }
+    //if email
+    if(!$_POST['email']){
+        //errors
+        $_SESSION['reg_errors'][]="Email is required.";
+    }
+    //else if filter var?
+    else if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+        //errors
+        $_SESSION['reg_errors'][]="Email is not valid.";
+    }
+    //if no password
+    if(!$_POST['password']){
+        //set error
+        $_SESSION['reg_errors'][]="Password is required.";
+    }
+    //else if strlen
+    else if($_POST['password']<=6){
+        //errors
+        $_SESSION['reg_errors'][]="Password must be greater than 6 characters.";
+    }
+    //if c_password
+    if(!$_POST['c_password']){
+        //errors
+        $_SESSION['reg_errors'][]="Password Confirmation is required.";
+    }
+    //else if c_password != password
+    else if($_POST['c_password']!=$_POST['password']){
+        //errors
+        $_SESSION['reg_errors'][]="Passwords must match.";
+    }
+//this is where I'll check the session errors
+    if(count($_SESSION['regerrors'])>0){
+        //got some errors send 'em back
+        header('location: index.php');
+        //kill it
+        die();
+    }
+    //else no errors check if email is taken
+    else{
+        //checking if email is taken
+        $query="SELECT id FROM email = '{$_POST['email']}'";
+        $user=fetch_record($query);
+        if(!empty($user)){
+            //setting session error
+            $_SESSION['reg_errors'][]="Email is taken.";
+            header('location: index.php');
+            die();
+        } else{
+        //escape malicious strings
+        $first_name=escape_this_string($_POST['first_name']);
+        $last_name=escape_this_string($_POST['last_name']);
+        $email=escape_this_string($_POST['email']);
+        $password=escape_this_string($_POST['password']);
+        //create a salt
+        $salt=bin2hex(openss1_random_psuedo_bytes(22));
+        //encrypt password with md5 & the salt
+        $enc_password=md5($password .''. $salt);
+
+        //finally query time!
+        $query = "INSERT INTO users (first_name, last_name, email, password, salt) VALUES ('{$first_name}', '{$last_name}', '{$email}', '{$enc_password}', '{$salt}')";
+        //set last_row_id, so query can run
+        $last_row_id=run_mysql_query($query);
+
+            if($last_row_id>0){
+                //saved successfully
+                $_SESSION['user_id']=$last_row_id;
+                $_SESSION['email']=$email;
+                header('location: success.php');
+                die();
+            }
+            //else kill it
+            else{
+            die('The INSERT failed. YOU failed!');
+            }
+        }
+    }
+
 //login form
+}else if(isset($_POST['action']) && $_POST['action'] == 'login'){
+    //don't let 'em mess up your db!
+    $email=escape_this_string($_POST['email']);
+    $password=escape_this_string($_POST['password']);
+    //find user based on email
+    $query="SELECT * FROM users WHERE email = '{$email}'";
+    $user=fetch_record($query);
+    //if user is found then we compare passwords and do encryption comparison
+    if(!empty($user)){
+        $enc_password=md5($password .''. $user['salt']);
+        if($user['password']==$enc_password){
+            //save the email and user_id in sesson
+            $_SESSION['user_id']=$user['id'];
+            $_SESSION['email']=$user['email'];
+            header('location: success.php');
+            die();
+        }
+        //else password didn't match
+        else{
+            $_SESSION['login_errors'][]="Email/Password Combination Failed";
+            header('location: index.php');
+            die();
+        }
+    }
+    //email not found, but don't tell them that
+    else{
+        $_SESSION['login_errors'][]="Email/Password Combination Failed";
+        header('location: index.php');
+        die();
+    }
+}
 //logout form
+else if(isset($_POST['action']) && $_POST['action']=='logout'){
+    session_destroy();
+    header('location: index.php');
+    die();
+}
+//no form
+else{
+    //someone on process.php w/o submitting a form
+    header('location: index.php');
+    die();
+}
 //new message form
 //new comment form
 //delete message form
