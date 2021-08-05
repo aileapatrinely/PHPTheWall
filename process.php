babf8aa74600859dd148870d25d7d1515521d439
Ailea Patrinely <aileapatrinely@gmail.com>
2021-08-05 15:44:27 -0500

    starts planning layout in code comments

--- process.php
+++ process.php
@@ -1,4 +1,13 @@
 <?php
 session_start();
-$_SESSION['errors']=[];
+require_once('new-connection.php');
+$_SESSION['reg-errors']=[];
+$_SESSION['login-errors']=[];
+
+//registration form
+//login form
+//logout form
+//new message form
+//new comment form
+//delete message form
 ?>
--- success.php
+++ success.php
@@ -5,10 +5,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Success</title>
+    <title>The Wall</title>
 </head>
 <body>
-
-</form>
+    <form>
+<--form for messages and comments-->
+    </form>
 </body>
 </html>
