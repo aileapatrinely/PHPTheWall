110fca6e3b17764e8142212fae847104e391db3a
Ailea Patrinely <aileapatrinely@gmail.com>
2021-08-06 10:09:01 -0500

    added code comments trying to keep track of all the divs and created message and date display

--- success.php
+++ success.php
@@ -10,32 +10,43 @@
 <body>
     <h4>Coding Dojo Wall</h4>
     <h4>Welcome <?= $_SESSION['first_name']?></h4>
-    <div>
-<--form for logout-->
+    <div> <!--1-->
+    <div><!--logout-->
+<!--form for logout-->
     <form action="process.php" method="post">
         <input type="hidden" name="action" value="logout">
         <input type="submit" value="Logging out? That's fine. No, I'm not upset.">
     </form>
-</div>
-<div>
-    <--form for messages-->
+</div><!--logout-->
+<div><!--messages-->
+    <!--form for messages-->
     <form action="process.php" method="post">
         <label for="message">Post a Message</label>
         <input type="hidden" name="action" value="new_message">
         <textarea id="messaage" name="message"></textarea>
         <input type="submit" value="You sure you want to say that? Yeah? Okay.">
 </form>
-<--this is where messages will display-->
+</div><!--messages form-->
+<!-- this is where messages will display--> 
 <?php foreach($_SESSION['messages'] as $message){?>
+    <div><!--outer-->
+        <div><!--inner-->
+            <h4><?= $message['first_name'] ?> <?= $message['last_name']?> 
+            - <?= date_format(date_create($message['created_at']), 'F jS Y') ?> </h4>
+            <p><?= $message['message'] ?></p>
+        <?php
+            $time_diff= date_diff(date_create($message['created_at']), date_create($now));
+            if (($message['user_id']==$_SESSION['user_id']) && ($time_dif->days < 1 && $time_diff->i <=30)){?>
+            <!--dis where da delete go-->
+            }
 <?php }?>
-<--date display?-->
-</div>
-<div>
-<--form for comments-->
+</div><!--inner-->
+</div><!--outer-->
+<!--form for comments-->
 <form action="process.php" method="post">
 </form>
-<--comment display-->
-<--dates?-->
-</div>
+<!--comment display-->
+<!--dates?-->
+</div><!--1-->
 </body>
 </html>
